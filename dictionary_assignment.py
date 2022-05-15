3ba9ac9d446b3b120368d8394ae75dc3c70c2422
tootang09137 <isk09137@naver.com>
2022-05-15 11:10:05 +0900

    list dic

---
+++ dictionary_assignment.py
@@ -0,0 +1,15 @@
+Dic1={'name' : '임민지', 'age' : '21', 'birthday' : '020506', 'gender' : '여', 'phone number' : '01066366287'}
+Dic2={'name' : '김종아', 'age' : '21', 'birthday' : '020205', 'gender' : '여', 'phone number' : '01099718402'}
+Dic3={'name' : '김민지', 'age' : '21', 'birthday' : '020204', 'gender' : '여', 'phone number' : '01040970963'}
+
+list1=[Dic1['name'], Dic2['name'], Dic3['name']]
+list2=[Dic1['age'], Dic2['age'], Dic3['age']]
+list3=[Dic1['birthday'],Dic2['birthday'],Dic3['birthday']]
+list4=[Dic1['gender'],Dic2['gender'],Dic3['gender']]
+list5=[Dic1['phone number'],Dic1['phone number'],Dic1['phone number']]
+
+print("이름 리스트는 ", list1)
+print("나이 리스트는 ", list2)
+print("생일 리스트는 ", list3)
+print("성별 리스트는 ", list4)
+print("전화번호 리스트는 ", list5)
