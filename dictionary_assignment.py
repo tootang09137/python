3d7dadd660a44330cc9ae4a19a6bd5678a1acd5c
tootang09137 <isk09137@naver.com>
2022-05-16 23:56:10 +0900

    first commit

--- dictionary_assignment.py
+++ dictionary_assignment.py
@@ -1,15 +1,21 @@
-Dic1={'name' : '임민지', 'age' : '21', 'birthday' : '020506', 'gender' : '여', 'phone number' : '01066366287'}
-Dic2={'name' : '김종아', 'age' : '21', 'birthday' : '020205', 'gender' : '여', 'phone number' : '01099718402'}
-Dic3={'name' : '김민지', 'age' : '21', 'birthday' : '020204', 'gender' : '여', 'phone number' : '01040970963'}
+Limminji={'name':'Limminji','age':'21','birthday':'020506','gender':'여자','phone_number':'01066366287'}
+Kimjongah={'name':'Kimjongah','age':'21','birthday':'020205','gender':'여자','phone_number':'01099718402'}
+Kimminji={'name':'Kimminji','age':'21','birthday':'020204','gender':'여자','phone_number':'01040970963'}
 
-list1=[Dic1['name'], Dic2['name'], Dic3['name']]
-list2=[Dic1['age'], Dic2['age'], Dic3['age']]
-list3=[Dic1['birthday'],Dic2['birthday'],Dic3['birthday']]
-list4=[Dic1['gender'],Dic2['gender'],Dic3['gender']]
-list5=[Dic1['phone number'],Dic1['phone number'],Dic1['phone number']]
+name=[1, 2, 3]
+age=[1, 2, 3]
+birthday=[1, 2, 3]
+gender =[1, 2, 3]
+phone_number =[1, 2, 3]
 
-print("이름 리스트는 ", list1)
-print("나이 리스트는 ", list2)
-print("생일 리스트는 ", list3)
-print("성별 리스트는 ", list4)
-print("전화번호 리스트는 ", list5)
+name[0:3]= [Limminji['name'] ,Kimjongah['name'],Kimminji['name']]
+age[0:3]= [Limminji['age'], Kimjongah['age'], Kimminji['age']]
+birthday[0:3]= [Limminji['birthday'], Kimjongah['birthday'], Kimminji['birthday']]
+gender[0:3]= [Limminji['gender'] ,Kimjongah['gender'] Kimminji['gender']]
+phone_number[0:3]= [Limminji['phone_number'] ,Kimjongah['phone_number'],Kimminji['phone_number']]
+
+print("이름 리스트는 %s" %name) 
+print("나이 리스트는 %s" %age )
+print("생일 리스트는 %s" %birthday)
+print("성별 리스트는 %s" %gender)
+print("전화번호 리스트는 %s" %phone_number)
--- list_assignment.py
+++ list_assignment.py
@@ -1,17 +1,21 @@
-alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",'']
-number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,'-']
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"," "]
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9","-"]
 
-print(alphabet[22], alphabet[4],alphabet[26], alphabet[0], alphabet[17], alphabet[4],alphabet[26],alphabet[18],alphabet[10],alphabet[7],alphabet[20],alphabet[26],alphabet[11],alphabet[8],alphabet[10],alphabet[4],alphabet[11],alphabet[8],alphabet[14],alphabet[13])
-print(alphabet[7], alphabet[0], alphabet[2], alphabet[10],alphabet[26],alphabet[24],alphabet[14],alphabet[20],alphabet[17],alphabet[26],alphabet[11],alphabet[8],alphabet[5],alphabet[4],alphabet[26])
-print(alphabet[12], alphabet[24], alphabet[26], alphabet[13],alphabet[0],alphabet[12],alphabet[4],alphabet[26],alphabet[8],alphabet[18],alphabet[26],alphabet[12],alphabet[8],alphabet[13],alphabet[9],alphabet[8])
+a=alphabet[22]+ alphabet[4]+alphabet[26]+ alphabet[0]+ alphabet[17]+ alphabet[4]+alphabet[26]+alphabet[18]+alphabet[10]+alphabet[7]+alphabet[20]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[10]+alphabet[4]+alphabet[11]+alphabet[8]+alphabet[14]+alphabet[13]
+b=alphabet[7]+ alphabet[0]+ alphabet[2]+ alphabet[10]+alphabet[26]+alphabet[24]+alphabet[14]+alphabet[20]+alphabet[17]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[5]+alphabet[4]+alphabet[26]
+c=alphabet[12]+ alphabet[24]+ alphabet[26]+ alphabet[13]+alphabet[0]+alphabet[12]+alphabet[4]+alphabet[26]+alphabet[8]+alphabet[18]+alphabet[26]+alphabet[12]+alphabet[8]+alphabet[13]+alphabet[9]+alphabet[8]
 
-birthday= [number[2], number[0], number[0], number[2], number[0], number[5], number[0], number[6]]
-phone_number= [number[0], number[1], number[0], number[10], number[6], number[6], number[3], number[6], number[10], number[6], number[2], number[8], number[7]]
-student_number= [number[2], number[0], number[2], number[1], number[1], number[2], number[0], number[5], number[6]]
+print(a)
+print(b)
+print(c)
 
-print("제 생일은 %s입니다." %birthday)
-print("제 전화번호는 %s 입니다." %phone_number)
-print("제 학번은 %s 입니다." %student_number)
+birthday= number[2]+ number[0]+ number[0]+ number[2]+ number[0]+ number[5]+number[0]+ number[6]
+phone_number= number[0]+ number[1]+ number[0]+ number[10]+ number[6]+ number[6]+number[3]+ number[6]+ number[10]+ number[6]+ number[2]+ number[8]+ number[7]
+student_number= number[2]+ number[0]+ number[2]+ number[1]+ number[1]+ number[2]+ number[0]+ number[5]+ number[6]
+
+print("제 생일은 %s 입니다. " % birthday)
+print("제 전화번호는 %s 입니다. " % phone_number)
+print("제 학번은 %s 입니다."  % student_number)
 
 
 
